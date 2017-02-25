18d8d0a9a67a608222f9fb55031d8c96846b7217
jncinlee <jncinlee@hotmail.com>
2017-02-25 17:02:57 +0100

    Update PysparkDecisionTree.py

--- PysparkDecisionTree.py
+++ PysparkDecisionTree.py
@@ -2,3 +2,7 @@ from pyspark.mllib.regression import LabeledPoint
 from pyspark.mllib.tree import DecisionTree
 from pyspark import SparkConf, SparkContext
 from numpy import array
+
+#set local machine and app name
+conf = SparkConf().setMaster("local").setAppName("SparkDecisionTree")
+sc = SparkContext(conf = conf)
